abb9aac94fd34e8c9b5791d89d4a7eaeae6e487c
Adam Nelson <adam.nelson102@gmail.com>
2015-03-30 15:15:09 +0100

    Update OOP RPS 3/30

--- oop_rps.rb
+++ oop_rps.rb
@@ -1,18 +1,18 @@
 class Hand
   attr_reader :choice
-  
+ 
   include Comparable
-  
+ 
   def initialize(choice)
     @choice = choice
   end
-  
+ 
   def <=>(other)
-    if @choice == other.choice
+    if choice == other.choice
       0
-    elsif @choice == "rock" && other.choice == "scissors" ||
-      @choice == "paper" && other.choice == "rock" ||
-      @choice == "scissors" && other.choice == "paper"
+    elsif choice == "rock" && other.choice == "scissors" ||
+      choice == "paper" && other.choice == "rock" ||
+      choice == "scissors" && other.choice == "paper"
       1
     else
       -1
@@ -25,16 +25,16 @@ class Player
 end
 
 class Human < Player
-  def initialize
+  def set_name
     self.name = gets.chomp
   end
-  
-  def choose
+ 
+  def choose_hand
     begin
       puts "Choose your weapon! (r/p/s)"
       choice = gets.chomp.downcase
     end until Game::CHOICES.keys.include?(choice)
-   
+  
     self.hand = Hand.new(Game::CHOICES.values_at(choice)[0])
   end
 end
@@ -42,6 +42,9 @@ end
 class Computer < Player
   def initialize
     self.name = "Computer"
+  end
+ 
+  def choose_hand
     self.hand = Hand.new(Game::CHOICES.values.sample)
   end
 end
@@ -52,11 +55,10 @@ class Game
   CHOICES = {"r" => "rock", "p" => "paper", "s" => "scissors"}
 
   def initialize
-    puts "Welcome to Rock, Paper, Scissors! Your name, please?"
     @human = Human.new
-    gameplay
+    @computer = Computer.new
   end
-  
+ 
   def compare_hands
     if human.hand > computer.hand
       winning_message(human.hand.choice)
@@ -68,38 +70,36 @@ class Game
       puts "Draw!"
     end
   end
-  
+ 
   def winning_message(winning_hand)
     case winning_hand
-    when "rock"
-      puts "Rock smashes scissors!"
-    when "paper"
-      puts "Paper covers rock!"
-    when "scissors"
-      puts "Scissors cuts paper!"
+    when "rock" then puts "Rock smashes scissors!"
+    when "paper" then puts "Paper covers rock!"
+    when "scissors" then puts "Scissors cuts paper!"
     end
   end
  
   def gameplay
-    human.choose
-    @computer = Computer.new
-    
-    puts "\n#{human.name} => #{human.hand.choice}"
-    puts "#{computer.name} => #{computer.hand.choice}\n "
-    
-    compare_hands
-    
-    begin
-      puts "\nGo again? (y/n)"
-      response = gets.chomp.downcase
-    end until ["y", "n"].include?(response)
-    if response == "y"
-      puts ""
-      gameplay
-    else
-      puts "\nGoodbye!"
+    puts "Welcome to Rock, Paper, Scissors! Your name, please?"
+    human.set_name
+    loop do
+      human.choose_hand
+      computer.choose_hand
+      puts "\n#{human.name} => #{human.hand.choice}"
+      puts "#{computer.name} => #{computer.hand.choice}\n "
+      compare_hands
+      begin
+        puts "\nGo again? (y/n)"
+        response = gets.chomp.downcase
+      end until ["y", "n"].include?(response)
+      if response == "y"
+        puts ""
+      else
+        puts "\nGoodbye!"
+        break
+      end
     end
   end
 end
 
-Game.new
+Game.new.gameplay
